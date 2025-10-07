530b2050ed46c40ee8dde2c4ddd237db3e12ff4d
deandoff <deandoff@gmail.com>
2025-10-07 15:23:15 +0300

    filter.kt

---
+++ Unit1/src/main/kotlin/u3_p1/higher_order_functions_with_collections/filter.kt
@@ -0,0 +1,12 @@
+package u3_p1.higher_order_functions_with_collections
+
+val softBakedMenu = cookies.filter {
+    it.softBaked
+}
+
+fun main() {
+    println("Soft cookies:")
+    softBakedMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
+}
