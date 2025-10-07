7e1c4498d598a876e267fd841e2ef183110f3fbe
deandoff <deandoff@gmail.com>
2025-10-07 15:16:54 +0300

    set.kt

---
+++ Unit1/src/main/kotlin/u3_p1/collections/set.kt
@@ -0,0 +1,15 @@
+package u3_p1.collections
+
+fun main() {
+    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println(solarSystem.size)
+    solarSystem.add("Pluto")
+    println(solarSystem.size)
+    println(solarSystem.contains("Pluto"))
+    solarSystem.add("Pluto")
+    println(solarSystem.size)
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
+    println(solarSystem.contains("Pluto"))
+
+}
