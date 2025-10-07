92814d481a94d733b1265156dec82ade1b4aff68
deandoff <deandoff@gmail.com>
2025-10-07 15:02:12 +0300

    singleton.kt

--- Unit1/src/main/kotlin/u3_p1/generics_objects_extensions/enum.kt
+++ Unit1/src/main/kotlin/u3_p1/generics_objects_extensions/enum.kt
@@ -3,3 +3,4 @@ package u3_p1.generics_objects_extensions
 enum class Difficulty {
     EASY, MEDIUM, HARD
 }
+
---
+++ Unit1/src/main/kotlin/u3_p1/generics_objects_extensions/singleton.kt
@@ -0,0 +1,16 @@
+package u3_p1.generics_objects_extensions
+
+class Quiz {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+fun main() {
+    println("${Quiz.answered} of ${Quiz.total} answered.")
+}
